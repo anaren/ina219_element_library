eb001e07f498005ffec4f60bdc7aee191799edc9
Nick Crast <nick.crast@anaren.com>
2017-07-13 14:25:09 -0400

    Running astyle. Fixing some errors that the Curie Compiler had. Curie compiler is quite strict.

--- driver/common/headers/ina219.h
+++ driver/common/headers/ina219.h
@@ -7,91 +7,93 @@
 /*=========================================================================
     CONFIG REGISTER (R/W)
     -----------------------------------------------------------------------*/
-    #define INA219_REG_CONFIG                      (0x00)
-    /*---------------------------------------------------------------------*/
-    #define INA219_CONFIG_RESET                    (0x8000)  // Reset Bit
-	
-    #define INA219_CONFIG_BVOLTAGERANGE_MASK       (0x2000)  // Bus Voltage Range Mask
-    #define INA219_CONFIG_BVOLTAGERANGE_16V        (0x0000)  // 0-16V Range
-    #define INA219_CONFIG_BVOLTAGERANGE_32V        (0x2000)  // 0-32V Range
-	
-    #define INA219_CONFIG_GAIN_MASK                (0x1800)  // Gain Mask
-    #define INA219_CONFIG_GAIN_1_40MV              (0x0000)  // Gain 1, 40mV Range
-    #define INA219_CONFIG_GAIN_2_80MV              (0x0800)  // Gain 2, 80mV Range
-    #define INA219_CONFIG_GAIN_4_160MV             (0x1000)  // Gain 4, 160mV Range
-    #define INA219_CONFIG_GAIN_8_320MV             (0x1800)  // Gain 8, 320mV Range
-	
-    #define INA219_CONFIG_BADCRES_MASK             (0x0780)  // Bus ADC Resolution Mask
-    #define INA219_CONFIG_BADCRES_9BIT             (0x0080)  // 9-bit bus res = 0..511
-    #define INA219_CONFIG_BADCRES_10BIT            (0x0100)  // 10-bit bus res = 0..1023
-    #define INA219_CONFIG_BADCRES_11BIT            (0x0200)  // 11-bit bus res = 0..2047
-    #define INA219_CONFIG_BADCRES_12BIT            (0x0400)  // 12-bit bus res = 0..4097
-    
-    #define INA219_CONFIG_SADCRES_MASK             (0x0078)  // Shunt ADC Resolution and Averaging Mask
-    #define INA219_CONFIG_SADCRES_9BIT_1S_84US     (0x0000)  // 1 x 9-bit shunt sample
-    #define INA219_CONFIG_SADCRES_10BIT_1S_148US   (0x0008)  // 1 x 10-bit shunt sample
-    #define INA219_CONFIG_SADCRES_11BIT_1S_276US   (0x0010)  // 1 x 11-bit shunt sample
-    #define INA219_CONFIG_SADCRES_12BIT_1S_532US   (0x0018)  // 1 x 12-bit shunt sample
-    #define INA219_CONFIG_SADCRES_12BIT_2S_1060US  (0x0048)	 // 2 x 12-bit shunt samples averaged together
-    #define INA219_CONFIG_SADCRES_12BIT_4S_2130US  (0x0050)  // 4 x 12-bit shunt samples averaged together
-    #define INA219_CONFIG_SADCRES_12BIT_8S_4260US  (0x0058)  // 8 x 12-bit shunt samples averaged together
-    #define INA219_CONFIG_SADCRES_12BIT_16S_8510US (0x0060)  // 16 x 12-bit shunt samples averaged together
-    #define INA219_CONFIG_SADCRES_12BIT_32S_17MS   (0x0068)  // 32 x 12-bit shunt samples averaged together
-    #define INA219_CONFIG_SADCRES_12BIT_64S_34MS   (0x0070)  // 64 x 12-bit shunt samples averaged together
-    #define INA219_CONFIG_SADCRES_12BIT_128S_69MS  (0x0078)  // 128 x 12-bit shunt samples averaged together
-	
-    #define INA219_CONFIG_MODE_MASK                (0x0007)  // Operating Mode Mask
-    #define INA219_CONFIG_MODE_POWERDOWN           (0x0000)
-    #define INA219_CONFIG_MODE_SVOLT_TRIGGERED     (0x0001)
-    #define INA219_CONFIG_MODE_BVOLT_TRIGGERED     (0x0002)
-    #define INA219_CONFIG_MODE_SANDBVOLT_TRIGGERED (0x0003)
-    #define INA219_CONFIG_MODE_ADCOFF              (0x0004)
-    #define INA219_CONFIG_MODE_SVOLT_CONTINUOUS    (0x0005)
-    #define INA219_CONFIG_MODE_BVOLT_CONTINUOUS    (0x0006)
-    #define INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS (0x0007)	
+#define INA219_REG_CONFIG                      (0x00)
+/*---------------------------------------------------------------------*/
+#define INA219_CONFIG_RESET                    (0x8000)  // Reset Bit
+
+#define INA219_CONFIG_BVOLTAGERANGE_MASK       (0x2000)  // Bus Voltage Range Mask
+#define INA219_CONFIG_BVOLTAGERANGE_16V        (0x0000)  // 0-16V Range
+#define INA219_CONFIG_BVOLTAGERANGE_32V        (0x2000)  // 0-32V Range
+
+#define INA219_CONFIG_GAIN_MASK                (0x1800)  // Gain Mask
+#define INA219_CONFIG_GAIN_1_40MV              (0x0000)  // Gain 1, 40mV Range
+#define INA219_CONFIG_GAIN_2_80MV              (0x0800)  // Gain 2, 80mV Range
+#define INA219_CONFIG_GAIN_4_160MV             (0x1000)  // Gain 4, 160mV Range
+#define INA219_CONFIG_GAIN_8_320MV             (0x1800)  // Gain 8, 320mV Range
+
+#define INA219_CONFIG_BADCRES_MASK             (0x0780)  // Bus ADC Resolution Mask
+#define INA219_CONFIG_BADCRES_9BIT             (0x0080)  // 9-bit bus res = 0..511
+#define INA219_CONFIG_BADCRES_10BIT            (0x0100)  // 10-bit bus res = 0..1023
+#define INA219_CONFIG_BADCRES_11BIT            (0x0200)  // 11-bit bus res = 0..2047
+#define INA219_CONFIG_BADCRES_12BIT            (0x0400)  // 12-bit bus res = 0..4097
+
+#define INA219_CONFIG_SADCRES_MASK             (0x0078)  // Shunt ADC Resolution and Averaging Mask
+#define INA219_CONFIG_SADCRES_9BIT_1S_84US     (0x0000)  // 1 x 9-bit shunt sample
+#define INA219_CONFIG_SADCRES_10BIT_1S_148US   (0x0008)  // 1 x 10-bit shunt sample
+#define INA219_CONFIG_SADCRES_11BIT_1S_276US   (0x0010)  // 1 x 11-bit shunt sample
+#define INA219_CONFIG_SADCRES_12BIT_1S_532US   (0x0018)  // 1 x 12-bit shunt sample
+#define INA219_CONFIG_SADCRES_12BIT_2S_1060US  (0x0048)	 // 2 x 12-bit shunt samples averaged together
+#define INA219_CONFIG_SADCRES_12BIT_4S_2130US  (0x0050)  // 4 x 12-bit shunt samples averaged together
+#define INA219_CONFIG_SADCRES_12BIT_8S_4260US  (0x0058)  // 8 x 12-bit shunt samples averaged together
+#define INA219_CONFIG_SADCRES_12BIT_16S_8510US (0x0060)  // 16 x 12-bit shunt samples averaged together
+#define INA219_CONFIG_SADCRES_12BIT_32S_17MS   (0x0068)  // 32 x 12-bit shunt samples averaged together
+#define INA219_CONFIG_SADCRES_12BIT_64S_34MS   (0x0070)  // 64 x 12-bit shunt samples averaged together
+#define INA219_CONFIG_SADCRES_12BIT_128S_69MS  (0x0078)  // 128 x 12-bit shunt samples averaged together
+
+#define INA219_CONFIG_MODE_MASK                (0x0007)  // Operating Mode Mask
+#define INA219_CONFIG_MODE_POWERDOWN           (0x0000)
+#define INA219_CONFIG_MODE_SVOLT_TRIGGERED     (0x0001)
+#define INA219_CONFIG_MODE_BVOLT_TRIGGERED     (0x0002)
+#define INA219_CONFIG_MODE_SANDBVOLT_TRIGGERED (0x0003)
+#define INA219_CONFIG_MODE_ADCOFF              (0x0004)
+#define INA219_CONFIG_MODE_SVOLT_CONTINUOUS    (0x0005)
+#define INA219_CONFIG_MODE_BVOLT_CONTINUOUS    (0x0006)
+#define INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS (0x0007)
 /*=========================================================================*/
 
 /*=========================================================================
     SHUNT VOLTAGE REGISTER (R)
     -----------------------------------------------------------------------*/
-    #define INA219_REG_SHUNTVOLTAGE                (0x01)
+#define INA219_REG_SHUNTVOLTAGE                (0x01)
 /*=========================================================================*/
 
 /*=========================================================================
     BUS VOLTAGE REGISTER (R)
     -----------------------------------------------------------------------*/
-    #define INA219_REG_BUSVOLTAGE                  (0x02)
+#define INA219_REG_BUSVOLTAGE                  (0x02)
 /*=========================================================================*/
 
 /*=========================================================================
     POWER REGISTER (R)
     -----------------------------------------------------------------------*/
-    #define INA219_REG_POWER                       (0x03)
+#define INA219_REG_POWER                       (0x03)
 /*=========================================================================*/
 
 /*=========================================================================
     CURRENT REGISTER (R)
     -----------------------------------------------------------------------*/
-    #define INA219_REG_CURRENT                     (0x04)
+#define INA219_REG_CURRENT                     (0x04)
 /*=========================================================================*/
 
 /*=========================================================================
     CALIBRATION REGISTER (R/W)
     -----------------------------------------------------------------------*/
-    #define INA219_REG_CALIBRATION                 (0x05)
+#define INA219_REG_CALIBRATION                 (0x05)
 /*=========================================================================*/
 
-typedef struct ina219_config_t {
-	uint16_t config_reg_val; /*< Configuration register raw value */
-	uint16_t calibration_reg_val; /*< Calibration register raw value */
-	uint16_t current_lsb_ma; /*< LSB of current reading. Used to convert from counts to mA */
+typedef struct ina219_config_t
+{
+    uint16_t config_reg_val; /*< Configuration register raw value */
+    uint16_t calibration_reg_val; /*< Calibration register raw value */
+    uint16_t current_lsb_ma; /*< LSB of current reading. Used to convert from counts to mA */
 } ina219_config_t;
 
-typedef enum ina219_result_t {
-	INA219_SUCCESS = 0,
-	INA219_INVALID_ADDR = 1,
-	INA219_I2C_WRITE = 2,
-	INA219_I2C_READ = 3,
+typedef enum ina219_result_t
+{
+    INA219_SUCCESS = 0,
+    INA219_INVALID_ADDR = 1,
+    INA219_I2C_WRITE = 2,
+    INA219_I2C_READ = 3,
 } ina219_result_t;
 
 /*
@@ -104,7 +106,7 @@ typedef enum ina219_result_t {
 *
 * @return ina219_result_t
 */
-ina219_result_t INA219_Init(uint8_t addr);
+ina219_result_t INA219_Init( uint8_t addr );
 
 /**
 * Write a raw INA219 Register
@@ -113,7 +115,7 @@ ina219_result_t INA219_Init(uint8_t addr);
 * @param[in] reg - register address
 * @param[in] value - value to write
 */
-ina219_result_t INA219_WriteReg(uint8_t addr, uint8_t reg, uint16_t value);
+ina219_result_t INA219_WriteReg( uint8_t addr, uint8_t reg, uint16_t value );
 
 /**
 * Read a raw INA219 Register
@@ -122,7 +124,7 @@ ina219_result_t INA219_WriteReg(uint8_t addr, uint8_t reg, uint16_t value);
 * @param[in] reg - register address
 * @param[out] value - value to read
 */
-ina219_result_t INA219_ReadReg(uint8_t addr, uint8_t reg, uint16_t *value);
+ina219_result_t INA219_ReadReg( uint8_t addr, uint8_t reg, uint16_t *value );
 
 /**
 * Set INA219 Configuration
@@ -132,7 +134,7 @@ ina219_result_t INA219_ReadReg(uint8_t addr, uint8_t reg, uint16_t *value);
 * @param[in] addr - I2C Address of device
 * @param[in] config
 */
-ina219_result_t INA219_SetConfig(uint8_t addr, const ina219_config_t *config);
+ina219_result_t INA219_SetConfig( uint8_t addr, const ina219_config_t *config );
 
 /**
 * Set INA219 Calibration
@@ -144,7 +146,7 @@ ina219_result_t INA219_SetConfig(uint8_t addr, const ina219_config_t *config);
 * @param[in] addr - I2C Address of device
 * @param[in] value - calculated calibration value
 */
-ina219_result_t INA219_SetCalibrationRaw(uint8_t addr, uint16_t value);
+ina219_result_t INA219_SetCalibrationRaw( uint8_t addr, uint16_t value );
 
 /**
 * Get direct reading of INA219_REG_BUSVOLTAGE register
@@ -152,7 +154,7 @@ ina219_result_t INA219_SetCalibrationRaw(uint8_t addr, uint16_t value);
 * @param[in] addr - I2C Address of device
 * @param[out] voltage
 */
-ina219_result_t INA219_GetBusVoltageRaw(uint8_t addr, int16_t *voltage);
+ina219_result_t INA219_GetBusVoltageRaw( uint8_t addr, int16_t *voltage );
 
 /**
 * Get direct reading of INA219_REG_SHUNTVOLTAGE register
@@ -160,7 +162,7 @@ ina219_result_t INA219_GetBusVoltageRaw(uint8_t addr, int16_t *voltage);
 * @param[in] addr - I2C Address of device
 * @param[out] voltage
 */
-ina219_result_t INA219_GetShuntVoltageRaw(uint8_t addr, int16_t *voltage);
+ina219_result_t INA219_GetShuntVoltageRaw( uint8_t addr, int16_t *voltage );
 
 /**
 * Get direct reading of INA219_REG_CURRENT register
@@ -168,7 +170,7 @@ ina219_result_t INA219_GetShuntVoltageRaw(uint8_t addr, int16_t *voltage);
 * @param[in] addr - I2C Address of device
 * @param[out] current
 */
-ina219_result_t INA219_GetCurrentRaw(uint8_t addr, int16_t *current);
+ina219_result_t INA219_GetCurrentRaw( uint8_t addr, int16_t *current );
 
 /**
 * Get Shunt Voltage in MilliVolts (mV)
@@ -176,7 +178,7 @@ ina219_result_t INA219_GetCurrentRaw(uint8_t addr, int16_t *current);
 * @param[in] addr - I2C Address of device
 * @param[out] voltage
 */
-ina219_result_t INA219_GetShuntVoltageMv(uint8_t addr, float *voltage);
+ina219_result_t INA219_GetShuntVoltageMv( uint8_t addr, float *voltage );
 
 /**
 * Get Bus Voltage in Volts (V)
@@ -184,7 +186,7 @@ ina219_result_t INA219_GetShuntVoltageMv(uint8_t addr, float *voltage);
 * @param[in] addr - I2C Address of device
 * @param[out] voltage
 */
-ina219_result_t INA219_GetBusVoltageV(uint8_t addr, float *voltage);
+ina219_result_t INA219_GetBusVoltageV( uint8_t addr, float *voltage );
 
 /**
 * Get Current in MilliAmps (mA)
@@ -192,4 +194,4 @@ ina219_result_t INA219_GetBusVoltageV(uint8_t addr, float *voltage);
 * @param[in] addr - I2C Address of device
 * @param[out] current
 */
-ina219_result_t INA219_GetCurrentMa(uint8_t addr, float *current);
+ina219_result_t INA219_GetCurrentMa( uint8_t addr, float *current );
--- driver/common/objects/ina219.c
+++ driver/common/objects/ina219.c
@@ -8,173 +8,183 @@
 
 static ina219_config_t ina219_config[NUM_IDS] = {0};
 
-static int8_t ina219_addr_to_index(uint8_t addr)
+static int8_t ina219_addr_to_index( uint8_t addr )
 {
-  switch(addr)
-  {
-    case 0x40:
-      return 0;
-    case 0x41:
-      return 1;
-    case 0x44:
-      return 2;
-    case 0x45:
-      return 3;
-    default:
-      return -1;
-  }
+    switch( addr )
+    {
+        case 0x40:
+            return 0;
+
+        case 0x41:
+            return 1;
+
+        case 0x44:
+            return 2;
+
+        case 0x45:
+            return 3;
+
+        default:
+            return -1;
+    }
 }
 
-static bool ina219_is_valid_addr(uint8_t addr)
+static bool ina219_is_valid_addr( uint8_t addr )
 {
-  return ina219_addr_to_index(addr) != -1;
+    return ina219_addr_to_index( addr ) != -1;
 }
 
-ina219_result_t INA219_Init(uint8_t addr)
+ina219_result_t INA219_Init( uint8_t addr )
 {
-  // NOTE: Default configuration assumes .1 Ohm shunt
-  // 32V 2A Range
-  // 10mA LSB for current readings
-  ina219_config_t default_config;
-  default_config.config_reg_val = INA219_CONFIG_BVOLTAGERANGE_32V |
-                    INA219_CONFIG_GAIN_8_320MV |
-                    INA219_CONFIG_BADCRES_12BIT |
-                    INA219_CONFIG_SADCRES_12BIT_1S_532US |
-                    INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS;
+    // NOTE: Default configuration assumes .1 Ohm shunt
+    // 32V 2A Range
+    // 10mA LSB for current readings
+    ina219_config_t default_config;
+    default_config.config_reg_val = INA219_CONFIG_BVOLTAGERANGE_32V |
+                                    INA219_CONFIG_GAIN_8_320MV |
+                                    INA219_CONFIG_BADCRES_12BIT |
+                                    INA219_CONFIG_SADCRES_12BIT_1S_532US |
+                                    INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS;
 
-  default_config.calibration_reg_val = 4096;
-  default_config.current_lsb_ma = 10;
-  ina219_result_t ret_val = INA219_SetConfig(addr, &default_config );
-  return ret_val;
+    default_config.calibration_reg_val = 4096;
+    default_config.current_lsb_ma = 10;
+    ina219_result_t ret_val = INA219_SetConfig( addr, &default_config );
+    return ret_val;
 }
 
-ina219_result_t INA219_WriteReg(uint8_t addr, uint8_t reg, uint16_t value)
+ina219_result_t INA219_WriteReg( uint8_t addr, uint8_t reg, uint16_t value )
 {
-  if( !ina219_is_valid_addr(addr) )
-    return;
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
 
-  uint8_t writeBytes[3];
+    uint8_t writeBytes[3];
 
-  writeBytes[0] = reg;
-  writeBytes[1] = value >> 8;
-  writeBytes[2] = value & 0xFF;
-  AIR_I2C_Write(addr, writeBytes, 3);
-  return INA219_SUCCESS;
+    writeBytes[0] = reg;
+    writeBytes[1] = value >> 8;
+    writeBytes[2] = value & 0xFF;
+    AIR_I2C_Write( addr, writeBytes, 3 );
+    return INA219_SUCCESS;
 }
 
-ina219_result_t INA219_ReadReg(uint8_t addr, uint8_t reg, uint16_t *value)
+ina219_result_t INA219_ReadReg( uint8_t addr, uint8_t reg, uint16_t *value )
 {
-  if( !ina219_is_valid_addr(addr) )
-    return 0;
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
 
-  uint8_t write_bytes[1] = {0};
-  uint8_t read_bytes[2] = {0};
-  uint16_t read_data = 0;
+    uint8_t write_bytes[1] = {0};
+    uint8_t read_bytes[2] = {0};
+    uint16_t read_data = 0;
 
-  write_bytes[0] = addr;
-  AIR_I2C_ComboRead(addr, write_bytes, 1, read_bytes, 2);
-  read_data = (unsigned int)read_bytes[0] << 8;
-  read_data |= read_bytes[1];
-  *value = read_data;
-  return INA219_SUCCESS;
+    write_bytes[0] = addr;
+    AIR_I2C_ComboRead( addr, write_bytes, 1, read_bytes, 2 );
+    read_data = ( unsigned int )read_bytes[0] << 8;
+    read_data |= read_bytes[1];
+    *value = read_data;
+    return INA219_SUCCESS;
 }
 
-ina219_result_t INA219_SetConfig(uint8_t addr, const ina219_config_t *config)
+ina219_result_t INA219_SetConfig( uint8_t addr, const ina219_config_t *config )
 {
-  if( !ina219_is_valid_addr(addr) )
-    return INA219_INVALID_ADDR;
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
 
-  ina219_config[ina219_addr_to_index(addr)] = *config;
-  ina219_result_t ret_val = INA219_WriteReg(addr, INA219_REG_CONFIG, config->config_reg_val);
+    ina219_config[ina219_addr_to_index( addr )] = *config;
+    ina219_result_t ret_val = INA219_WriteReg( addr, INA219_REG_CONFIG, config->config_reg_val );
 
-  if( ret_val == INA219_SUCCESS )
-  {
-    ret_val = INA219_SetCalibrationRaw(addr, config->calibration_reg_val);
-  }
+    if( ret_val == INA219_SUCCESS )
+    {
+        ret_val = INA219_SetCalibrationRaw( addr, config->calibration_reg_val );
+    }
 
-  return ret_val;
+    return ret_val;
 }
 
 
-ina219_result_t INA219_SetCalibrationRaw(uint8_t addr, uint16_t value)
+ina219_result_t INA219_SetCalibrationRaw( uint8_t addr, uint16_t value )
 {
-if( !ina219_is_valid_addr(addr) )
-    return INA219_INVALID_ADDR;
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
 
-  ina219_config[ina219_addr_to_index(addr)].calibration_reg_val = value;
-  return INA219_WriteReg(addr, INA219_REG_CALIBRATION, value);
+    ina219_config[ina219_addr_to_index( addr )].calibration_reg_val = value;
+    return INA219_WriteReg( addr, INA219_REG_CALIBRATION, value );
 }
 
-ina219_result_t INA219_GetBusVoltageRaw(uint8_t addr, int16_t *voltage) {
-if( !ina219_is_valid_addr(addr) )
-    return INA219_INVALID_ADDR;
+ina219_result_t INA219_GetBusVoltageRaw( uint8_t addr, int16_t *voltage )
+{
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
 
-  uint16_t value = 0;
+    uint16_t value = 0;
 
-  ina219_result_t ret_val = INA219_ReadReg(addr, INA219_REG_BUSVOLTAGE, &value);
+    ina219_result_t ret_val = INA219_ReadReg( addr, INA219_REG_BUSVOLTAGE, &value );
 
-  // Shift to the right 3 to drop CNVR and OVF and multiply by LSB
-  *voltage = (int16_t)((value >> 3) * 4);
+    // Shift to the right 3 to drop CNVR and OVF and multiply by LSB
+    *voltage = ( int16_t )( ( value >> 3 ) * 4 );
 
-  return ret_val;
+    return ret_val;
 }
 
-ina219_result_t INA219_GetShuntVoltageRaw(uint8_t addr, int16_t *voltage) {
-if( !ina219_is_valid_addr(addr) )
-    return INA219_INVALID_ADDR;
+ina219_result_t INA219_GetShuntVoltageRaw( uint8_t addr, int16_t *voltage )
+{
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
 
-  return INA219_ReadReg(addr, INA219_REG_SHUNTVOLTAGE, voltage);
+    return INA219_ReadReg( addr, INA219_REG_SHUNTVOLTAGE, ( uint16_t * )voltage );
 }
 
 /**************************************************************************/
-/*! 
+/*!
     @brief  Gets the raw current value (16-bit signed integer, so +-32767)
 */
 /**************************************************************************/
-ina219_result_t INA219_GetCurrentRaw(uint8_t addr, int16_t *current) {
-if( !ina219_is_valid_addr(addr) )
-    return INA219_INVALID_ADDR;
+ina219_result_t INA219_GetCurrentRaw( uint8_t addr, int16_t *current )
+{
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
 
-  // Sometimes a sharp load will reset the INA219, which will
-  // reset the cal register, meaning CURRENT and POWER will
-  // not be available ... avoid this by always setting a cal
-  // value even if it's an unfortunate extra step
-  ina219_result_t ret_val = INA219_WriteReg(addr, INA219_REG_CALIBRATION, ina219_config[ina219_addr_to_index(addr)].calibration_reg_val);
+    // Sometimes a sharp load will reset the INA219, which will
+    // reset the cal register, meaning CURRENT and POWER will
+    // not be available ... avoid this by always setting a cal
+    // value even if it's an unfortunate extra step
+    ina219_result_t ret_val = INA219_WriteReg( addr, INA219_REG_CALIBRATION, ina219_config[ina219_addr_to_index( addr )].calibration_reg_val );
 
-  if( ret_val == INA219_SUCCESS )
-  {
-    ret_val = INA219_ReadReg(addr, INA219_REG_CURRENT, current);
-  }
-  
-  return ret_val;
-}
- 
-ina219_result_t INA219_GetShuntVoltageMv(uint8_t addr, float *voltage) {
-if( !ina219_is_valid_addr(addr) )
-    return INA219_INVALID_ADDR;
+    if( ret_val == INA219_SUCCESS )
+    {
+        ret_val = INA219_ReadReg( addr, INA219_REG_CURRENT, ( uint16_t * )current );
+    }
 
-  int16_t value;
-  ina219_result_t ret_val = INA219_GetShuntVoltageRaw(addr, &value);
-  *voltage = value * 0.01;
-  return ret_val;
+    return ret_val;
 }
 
-ina219_result_t INA219_GetBusVoltageV(uint8_t addr, float *voltage) {
-if( !ina219_is_valid_addr(addr) )
-    return INA219_INVALID_ADDR;
+ina219_result_t INA219_GetShuntVoltageMv( uint8_t addr, float *voltage )
+{
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
 
-  int16_t value;
-  ina219_result_t ret_val = INA219_GetBusVoltageRaw(addr, &value);
-  *voltage = value * 0.001;
-  return ret_val;
+    int16_t value;
+    ina219_result_t ret_val = INA219_GetShuntVoltageRaw( addr, &value );
+    *voltage = value * 0.01;
+    return ret_val;
 }
 
-ina219_result_t INA219_GetCurrentMa(uint8_t addr, float *current) {
-if( !ina219_is_valid_addr(addr) )
-    return INA219_INVALID_ADDR;
+ina219_result_t INA219_GetBusVoltageV( uint8_t addr, float *voltage )
+{
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
 
-  int16_t value_dec;
-  ina219_result_t ret_val = INA219_GetCurrentRaw(addr, &value_dec);
-  *current = value_dec / ina219_config[ina219_addr_to_index(addr)].current_lsb_ma;
-  return ret_val;
+    int16_t value;
+    ina219_result_t ret_val = INA219_GetBusVoltageRaw( addr, &value );
+    *voltage = value * 0.001;
+    return ret_val;
+}
+
+ina219_result_t INA219_GetCurrentMa( uint8_t addr, float *current )
+{
+    if( !ina219_is_valid_addr( addr ) )
+        return INA219_INVALID_ADDR;
+
+    int16_t value_dec;
+    ina219_result_t ret_val = INA219_GetCurrentRaw( addr, &value_dec );
+    *current = value_dec / ina219_config[ina219_addr_to_index( addr )].current_lsb_ma;
+    return ret_val;
 }
